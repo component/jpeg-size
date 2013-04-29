b70d1655c63acb11c168d65f4d2ed01264ef6a99
TJ Holowaychuk <tj@vision-media.ca>
2013-04-29 14:49:56 -0700

    add optional [path] to example

--- example.js
+++ example.js
@@ -6,6 +6,9 @@
 var fs = require('fs');
 var size = require('./');
 
+// [path]
+var path = process.argv[3] || 'droplet.jpg';
+
 // 473 x 315
-var buf = fs.readFileSync('droplet.jpg');
+var buf = fs.readFileSync(path);
 console.log(size(buf));
